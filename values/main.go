3865d23d2fba9c13471f8e66fa2ccf81df99e5dc
aniruddhrane <aniruddhrane5@gmail.com>
2026-05-25 17:08:02 +0530

    Learned values/literals

---
+++ values/main.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("go"+"lang")
+	fmt.Println("1+1 =",1+1)
+	fmt.Println("7.0/3.0=",7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true||false)
+	fmt.Println(!true)
+}
+/*
+  Go has various values types including strings,integers,
+  floats,booleans,etc. Above are few basic example
+
+  Strings which can be added together with +.
+  Integers and floats.
+  Booleans,with boolean operators as you'd expect.
+*/
